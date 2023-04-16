f3464a609b47460f03a89fee64c1bae6b577980d
Tushar Garg <tushar7258.ca22@chitkara.edu.in>
2023-04-16 16:17:10 +0530

    Update Assignment9.c

--- Assignment9.c
+++ Assignment9.c
@@ -5,8 +5,35 @@
 //.       m = 1
 //.       n = 1
 
+#include<stdio.h>
+#include<string.h>
 int main()
 {
-  
-  return 0;
+    char str[100];
+    int count;
+    printf("Enter the string : ");
+    gets(str);
+    for(int i=0;i<strlen(str);i++)
+    {
+        int a=1;
+        count=0;
+        for(int j=0;j<i;j++)
+        {
+            if(str[i]==str[j] && i!=j)
+            {
+                a=0;
+                break;
+            }
+        }
+        if(a==1)
+        {
+            for(int j=0;j<strlen(str);j++)
+            {
+                if(str[i]==str[j])
+                    count++;
+            }
+            printf("%c : %d\n",str[i],count);
+        }
+    }
+	return 0;
 }
