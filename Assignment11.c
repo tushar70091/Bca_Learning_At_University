b0c70dc41ec35dda653af00fe0458c0455c964a6
Tushar Garg <tushar7258.ca22@chitkara.edu.in>
2023-04-16 16:41:45 +0530

    Update Assignment11.c

--- Assignment11.c
+++ Assignment11.c
@@ -1,8 +1,17 @@
 //Convert an decimal to its binary equivalent using a bitwise operator
 
-int main()
-{
-  int N = 7;
-  
-  return 0;
+#include <stdio.h>
+int main() {
+    int decimal,binary=0, base=1,rem;
+    printf("Enter a decimal number: ");
+    scanf("%d",&decimal);
+    while(decimal>0) 
+	{
+        rem=decimal&1;
+        binary=binary+(rem*base);
+        base=base*10;
+        decimal>>=1;
+    }
+    printf("Binary equivalent: %d",binary);
+    return 0;
 }
